cc2e537891e9bc4ea4c8e99383312b194dc63440
Christian Mendez <118922501+cmendez-ref@users.noreply.github.com>
2025-09-02 13:50:11 -0500

    Fix migrations

--- database/generator/AbstractTemplateGenerator.php
+++ database/generator/AbstractTemplateGenerator.php
@@ -1,110 +1,67 @@
 <?php
 
 use Phinx\Migration\CreationInterface;
+use Phinx\Util\Util;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-/**
- * Class Migration
- *
- * @package App\Database\Migrations
- */
 abstract class AbstractTemplateGenerator implements CreationInterface
 {
-    
-    const TEMPLATE_DIRECTORY = __DIR__ . '/templates/';
-    
+    /** @var InputInterface */
+    private $input;
+    /** @var OutputInterface */
+    private $output;
+
     /**
-     * @var null|\Symfony\Component\Console\Input\InputInterface
+     * {@inheritdoc}
      */
-    protected $input;
+    public function __construct(?InputInterface $input = null, ?OutputInterface $output = null)
+    {
+        if ($input) {
+            $this->setInput($input);
+        }
+        if ($output) {
+            $this->setOutput($output);
+        }
+    }
+
     /**
-     * @var null|\Symfony\Component\Console\Output\OutputInterface
+     * {@inheritdoc}
      */
-    protected $output;
-    
-    protected $table;
-    
+    public function postCreate(string $migrationPath, string $className, string $baseClassName): void
+    {
+        // Do nothing
+    }
+
     /**
-     * CreationInterface constructor.
-     *
-     * @param \Symfony\Component\Console\Input\InputInterface|null   $input
-     * @param \Symfony\Component\Console\Output\OutputInterface|null $output
+     * @return InputInterface
      */
-    public function __construct(InputInterface $input = null, OutputInterface $output = null)
+    public function getInput(): InputInterface
+    {
+        return $this->input;
+    }
+
+    /**
+     * @param InputInterface $input
+     */
+    public function setInput(InputInterface $input): void
     {
         $this->input = $input;
-        $this->output = $output;
-        $this->getParams();
     }
-    
-    /**
-     * @param \Symfony\Component\Console\Input\InputInterface $input
-     *
-     * @return \Phinx\Migration\CreationInterface
-     */
-    public function setInput(InputInterface $input)
-    {
-        // TODO: Implement setInput() method.
-    }
-    
-    /**
-     * @param \Symfony\Component\Console\Output\OutputInterface $output
-     *
-     * @return \Phinx\Migration\CreationInterface
-     */
-    public function setOutput(OutputInterface $output)
-    {
-        // TODO: Implement setOutput() method.
-    }
-    
-    /**
-     * @return \Symfony\Component\Console\Input\InputInterface
-     */
-    public function getInput()
-    {
-        // TODO: Implement getInput() method.
-    }
-    
-    /**
-     * @return \Symfony\Component\Console\Output\OutputInterface
-     */
-    public function getOutput()
-    {
-        // TODO: Implement getOutput() method.
-    }
-    
-    /**
-     * Get the migration template.
-     *
-     * This will be the content that Phinx will amend to generate the migration file.
-     *
-     * @return string The content of the template for Phinx to amend.
-     */
-    public function getMigrationTemplate()
-    {
-        // TODO: Implement getMigrationTemplate() method.
-    }
-    
-    /**
-     * Post Migration Creation.
-     *
-     * Once the migration file has been created, this method will be called, allowing any additional
-     * processing, specific to the template to be performed.
-     *
-     * @param string $migrationFilename The name of the newly created migration.
-     * @param string $className         The class name.
-     * @param string $baseClassName     The name of the base class.
-     *
-     * @return void
-     */
-    public function postMigrationCreation($migrationFilename, $className, $baseClassName)
-    {
-        // TODO: Implement postMigrationCreation() method.
-    }
-    
-    private function getParams()
-    {
 
+    /**
+     * @return OutputInterface
+     */
+    public function getOutput(): OutputInterface
+    {
+        return $this->output;
+    }
+
+    /**
+     * @param OutputInterface $output
+     */
+    public function setOutput(OutputInterface $output): void
+    {
+        $this->output = $output;
     }
 }
--- database/generator/TemplateGenerator.php
+++ database/generator/TemplateGenerator.php
@@ -2,12 +2,27 @@
 
 class TemplateGenerator extends AbstractTemplateGenerator
 {
-    
-    public function getMigrationTemplate()
+    /**
+     * {@inheritdoc}
+     */
+    public function getMigrationTemplate(): string
     {
-        return file_get_contents(static::TEMPLATE_DIRECTORY . 'defaultTemplate.dist');
+        return file_get_contents(__DIR__ . '/../templates/migration.stub');
     }
-    
-    
-    
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getSeedTemplate(): string
+    {
+        return file_get_contents(__DIR__ . '/../templates/seed.stub');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function postMigrationCreation(string $migrationPath, string $className, string $baseClassName): void
+    {
+        // Do nothing, just satisfy the interface
+    }
 }
